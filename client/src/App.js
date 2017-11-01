1b297f9638b6485697df1ef3e65c7dfd66a3d6a5
gp22 <gp22@tutanota.com>
2017-10-31 20:14:37 -0400

    Add base React files

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import logo from './logo.svg';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Nav />
+        <Body />
+        <Footer />
+      </div>
+    );
+  }
+}
---
+++ client/src/components/Nav.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Nav extends Component {
+  render() {
+    return (
+      <nav>
+        <h1>Nav!</h1>
+      </nav>
+    );
+  }
+}
